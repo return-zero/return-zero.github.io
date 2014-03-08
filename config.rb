5876a4c7c78f6a56db985f4888aab17705a2b0e3
Masaaki Morishita <ikasama3.0@gmail.com>
2014-03-09 06:11:09 +0900

    Configurate compass

--- config.rb
+++ config.rb
@@ -3,9 +3,9 @@
 ###
 
 # Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+compass_config do |config|
+  config.output_style = :compact
+end
 
 ###
 # Page options, layouts, aliases and proxies
