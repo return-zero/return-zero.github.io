89118b09d3e11f71ab511dbb0180425ec5cd762a
Masaaki Morishita <ikasama3.0@gmail.com>
2014-10-21 01:14:09 +0900

    random profile link

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,17 @@
 //= require_tree .
+
+$(function () {
+    var arr = [];
+    $('.clearfix').each(function() {
+        arr.push($(this).html());
+    });
+
+    arr.sort(function() {
+        return Math.random() - Math.random();
+    });
+
+    $('.clearfix').empty();
+    for (var i = 0; i < arr.length; i++) {
+        $('clearfix').append(arr[i]);
+    }
+});
