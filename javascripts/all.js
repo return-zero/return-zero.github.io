ea20866d732faf8f900133b4dedb423ba34c7125
Tomohiro <neoen.gsn@gmail.com>
2015-02-01 09:33:35 +0000

    Update from Travis CI

---
+++ javascripts/all.js
@@ -0,0 +1,1 @@
+$(function(){var e=[];$(".member div").each(function(){e.push($(this).html())}),e.sort(function(){return Math.random()-Math.random()}),$(".member").empty();for(var t=0;t<e.length;t++)$(".member").append('<div class="col-xs-12 col-md-6 col-lg-3 text-center">'+e[t]+"</div>")});
